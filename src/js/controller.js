c988341e2cd46ae789adfdbcd1ed5649f8c90417
karthickeyan <jkarthickeyan24@gmail.com>
2024-12-20 14:56:22 +0530

    added loadRecipe function

--- src/js/controller.js
+++ src/js/controller.js
@@ -8,8 +8,31 @@ const timeout = function (s) {
   });
 };
 
-// https://forkify-api.herokuapp.com/v2
-
 ///////////////////////////////////////
 
-console.log('test');
+const loadRecipe = async function () {
+  try {
+    const res = await fetch(
+      'https://forkify-api.jonas.io/api/v2/recipes/5ed6604591c37cdc054bc886'
+    );
+    const data = await res.json();
+    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+    console.log(res, data);
+    let { recipe } = data.data;
+    recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceURL: recipe.source_url,
+      image: recipe.image_url,
+      cookingTime: recipe.cooking_time,
+      servings: recipe.servings,
+      ingredients: recipe.ingredients,
+    };
+    console.log(recipe);
+  } catch (err) {
+    console.log(err.message);
+  }
+};
+
+loadRecipe();
