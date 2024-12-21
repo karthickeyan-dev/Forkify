d8af839a00feb105f6ef79f60ede50860ca6b196
karthickeyan <jkarthickeyan24@gmail.com>
2024-12-21 16:25:05 +0530

    feat: add configuration for API URL and timeout; refactor recipe loading with helper functions

--- src/js/model.js
+++ src/js/model.js
@@ -1,24 +1,27 @@
+import { API_URL } from './config';
+import { getJSON } from './helpers';
+
 const state = {
   recipe: {},
 };
 
 export const loadRecipe = async function (id) {
-  const res = await fetch(`https://forkify-api.jonas.io/api/v2/recipes/${id}`);
-  const data = await res.json();
-  if (!res.ok) throw new Error(`${data.message} (${res.status})`);
-
-  // console.log(res, data);
-  const { recipe } = data.data;
-  state.recipe = {
-    id: recipe.id,
-    title: recipe.title,
-    publisher: recipe.publisher,
-    sourceURL: recipe.source_url,
-    image: recipe.image_url,
-    cookingTime: recipe.cooking_time,
-    servings: recipe.servings,
-    ingredients: recipe.ingredients,
-  };
-  // console.log(state.recipe);
-  return state.recipe;
+  try {
+    const data = await getJSON(`${API_URL}${id}`);
+    const { recipe } = data.data;
+    state.recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceURL: recipe.source_url,
+      image: recipe.image_url,
+      cookingTime: recipe.cooking_time,
+      servings: recipe.servings,
+      ingredients: recipe.ingredients,
+    };
+    // console.log(state.recipe);
+    return state.recipe;
+  } catch (err) {
+    console.error(`🚫🚫🚫${err}🚫🚫🚫`);
+  }
 };
